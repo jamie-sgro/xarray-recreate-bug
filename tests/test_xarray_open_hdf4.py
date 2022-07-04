2e68bfa516a6c2f17ff12a1f0ffcf9c74bd7ad51
Jamie Sgro <j.sgro@fraym.io>
2022-07-04 13:54:33 -0400

    updated test names and docstr

--- tests/test_xarray_open_hdf4.py
+++ tests/test_xarray_open_hdf4.py
@@ -43,19 +43,8 @@ def test_can_open_hdf4_closer_to_error_replication(tmp_path: Path):
                 layer.append(da)
         rtn.append(layer)
 
-def test2(tmp_path: Path):
-    """A Unique bug discovered June 22nd 2022.
-    In Docker environments only, throws the below error. This only occurs
-    when trying to append the output of open_hdf4 to another object, and so
-    far only fails on the 3rd iteration, regardless of the order of the files
-    and the contents of the files themselves. Note we use a copy of a file
-    for each iteration and it still fails
-
-    ```bash
-    rasterio.errors.RasterioIOError: HDF4_EOS:EOS_GRID:/tmp/pytest-of-root/
-    pytest-5/test_can_open_hdf4_closer_to_e0/file3:MODIS_Grid_16DAY_1km_VI:1
-    km 16 days blue reflectance: No such file or directory
-    ```
+def test_using_xarray_via_rioxarray(tmp_path: Path):
+    """Same as above but using the rioxaray library to open via rasterio
     """
 
     num_files = 4
@@ -64,17 +53,6 @@ def test2(tmp_path: Path):
     for i in range(num_files):
         shutil.copyfile(FILEPATH, filepaths[i])
 
-
-    rtn = []
     for filepath in filepaths:
-        with xr.open_dataset(filepath, engine="rasterio") as ds_new:
+        with xr.open_dataset(filepath, engine="rasterio") as _:
             ...
-        # with warnings.catch_warnings():
-        #     warnings.simplefilter("ignore", category=NotGeoreferencedWarning)
-        #     with rasterio.open(filepath) as src:
-        #         layer_names = src.subdatasets
-        # layer = []
-        # for layer_name in layer_names:
-        #     with xr.open_rasterio(layer_name) as da:
-        #         layer.append(da)
-        # rtn.append(layer)
